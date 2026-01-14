dbfa0cb9e9f48756f547b98f19539355d6057cd7
unknown <gogavlad23@stud.ase.ro>
2026-01-15 01:03:04 +0200

    curatare cai

--- proiect/backend/src/server.js
+++ proiect/backend/src/server.js
@@ -51,10 +51,10 @@ const startServer = async () => {
     console.error("❌ Eroare la pornirea serverului:", error);
   }
 };
-app.use('/api/users', userRoutes);
-app.use('/api/tasks', taskRoutes);
-app.use('/api/teams', teamRoutes);
-app.use('/api/auth', authRoutes);
-app.use('/api/admin', adminRoutes);
+app.use('/users', userRoutes);
+app.use('/tasks', taskRoutes);
+app.use('/teams', teamRoutes);
+app.use('/auth', authRoutes);
+app.use('/admin', adminRoutes);
 
 startServer();
